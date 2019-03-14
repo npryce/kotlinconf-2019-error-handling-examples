15c1a1da81756789caa6e490a8e5bfc1db67d0d1
Nat Pryce <sw@natpryce.com>
2019-03-14 08:46:05 +0000

    Examples of distinguishing beween error and absence when using nulls to represent error cases

--- src/example/nullable_example.kt
+++ src/example/nullable_example.kt
@@ -10,24 +10,34 @@ import java.time.format.DateTimeParseException
 
 
 fun DateTimeFormatter.parseInstant(s: String) =
-        try {
-            parse(s, Instant::from)
-        }
-        catch (e: DateTimeParseException) {
-            null
-        }
+    try {
+        parse(s, Instant::from)
+    } catch (e: DateTimeParseException) {
+        null
+    }
 
 
-fun handleGet(request: HttpRequest): HttpResponse {
-    val startTime = request["from"].firstOrNull()?.let {
-        ISO_INSTANT.parseInstant(it)
-            ?: return HttpResponse(HTTP_BAD_REQUEST).body("invalid from time")
-    } ?: Instant.now()
+fun handleGet_simple(request: HttpRequest): HttpResponse {
+    val count = request["count"].firstOrNull()?.toIntOrNull()
+        ?: return HttpResponse(HTTP_BAD_REQUEST).body("invalid count")
 
+    val startTime = request["from"].firstOrNull()
+        ?.let { ISO_INSTANT.parseInstant(it) }
+        ?: return HttpResponse(HTTP_BAD_REQUEST).body("invalid from time")
+
+    etcetera
+}
+
+fun handleGet_correct(request: HttpRequest): HttpResponse {
     val count = request["count"].firstOrNull()?.let {
         it.toIntOrNull()
             ?: return HttpResponse(HTTP_BAD_REQUEST).body("invalid count")
     } ?: 100
+
+    val startTime = request["from"].firstOrNull()?.let {
+        ISO_INSTANT.parseInstant(it)
+            ?: return HttpResponse(HTTP_BAD_REQUEST).body("invalid from time")
+    } ?: Instant.now()
 
     etcetera
 }
