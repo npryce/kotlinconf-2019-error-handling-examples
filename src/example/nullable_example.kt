fe87dc114ff3518b2637f5a4ff672180eaaba2e3
Nat Pryce <sw@natpryce.com>
2019-03-15 11:44:09 +0000

    Extension functions

--- src/example/nullable_example.kt
+++ src/example/nullable_example.kt
@@ -16,7 +16,6 @@ fun DateTimeFormatter.parseInstant(s: String): Instant? =
         null
     }
 
-
 fun handleGet_simple(request: HttpRequest): HttpResponse {
     val count = request["count"].firstOrNull()?.toIntOrNull()
         ?: return HttpResponse(HTTP_BAD_REQUEST).body("invalid count")
@@ -27,6 +26,26 @@ fun handleGet_simple(request: HttpRequest): HttpResponse {
 
     etcetera
 }
+
+fun String.toInstantOrNull(format: DateTimeFormatter): Instant? =
+    try {
+        format.parse(this, Instant::from)
+    } catch (e: DateTimeParseException) {
+        null
+    }
+
+
+fun handleGet_simple_with_extension_fun(request: HttpRequest): HttpResponse {
+    val count = request["count"].firstOrNull()?.toIntOrNull()
+        ?: return HttpResponse(HTTP_BAD_REQUEST).body("invalid count")
+
+    val startTime = request["from"].firstOrNull()?.toInstantOrNull(ISO_INSTANT)
+        ?: return HttpResponse(HTTP_BAD_REQUEST).body("invalid from time")
+
+    etcetera
+}
+
+
 
 fun handleGet_correct(request: HttpRequest): HttpResponse {
     val count = request["count"].firstOrNull()?.let {
@@ -41,4 +60,6 @@ fun handleGet_correct(request: HttpRequest): HttpResponse {
 
     etcetera
 }
+
+
 
